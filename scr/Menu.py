264dfad753c5b31d5edf18f9d1436a5826f06dfe
JessielCH <150202777+JessielCH@users.noreply.github.com>
2024-02-14 12:09:44 -0500

    Actualizar Menu.py

--- scr/Menu.py
+++ scr/Menu.py
@@ -1,4 +1,5 @@
 from mini_proyecto_monitoreo_videojuegos.scr.ProgramMonitor import ProgramMonitor
+
 class Menu:
     def __init__(self):
         self.PROGRAM_MONITOR = ProgramMonitor()
@@ -8,8 +9,9 @@ class Menu:
         print("2. Eliminar programa de la lista de monitoreo")
         print("3. Mostrar lista de programas en monitoreo")
         print("4. Iniciar monitoreo")
-        print("5. Dejar de monitorear")
-        print("6. Salir")
+        print("5. Detener monitoreo")
+        print("6. Activar/Desactivar monitoreo")
+        print("7. Salir")
 
     def add_program_to_monitor_list(self):
         program_name = input("Ingrese el nombre del programa: ")
@@ -23,9 +25,12 @@ class Menu:
         else:
             print(f"{program_name} no se encontró en la lista de monitoreo.")
 
-    def show_monitoring_list(self):
-        print("Lista de programas en monitoreo:")
-        for program in self.PROGRAM_MONITOR.PROGRAMS_TO_LOG:
+    def show_monitoring_list(self, page=1, items_per_page=5):
+        start_index = (page - 1) * items_per_page
+        end_index = start_index + items_per_page
+
+        print(f"Lista de programas en monitoreo (Página {page}):")
+        for program in self.PROGRAM_MONITOR.PROGRAMS_TO_LOG[start_index:end_index]:
             print(f"- {program}")
 
     def start_monitoring(self):
@@ -34,6 +39,14 @@ class Menu:
     def stop_monitoring(self):
         print("Deteniendo el monitoreo...")
         exit()
+
+    def toggle_monitoring(self):
+        if self.PROGRAM_MONITOR.is_monitoring_enabled:
+            print("Desactivando el monitoreo...")
+            self.PROGRAM_MONITOR.disable_monitoring()
+        else:
+            print("Activando el monitoreo...")
+            self.PROGRAM_MONITOR.enable_monitoring()
 
     def execute_option(self, option):
         if option == "1":
@@ -47,6 +60,8 @@ class Menu:
         elif option == "5":
             self.stop_monitoring()
         elif option == "6":
+            self.toggle_monitoring()
+        elif option == "7":
             print("Saliendo del programa...")
             exit()
         else:
