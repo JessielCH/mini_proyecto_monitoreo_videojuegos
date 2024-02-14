78f6ca66b08b16c80a3ae37d77be185429e1c4a9
JessielCH <150202777+JessielCH@users.noreply.github.com>
2024-02-14 12:11:38 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-from scr.Menu import Menu
-from scr.ProgramMonitor import ProgramMonitor
+from mini_proyecto_monitoreo_videojuegos.scr.ProgramMonitor import ProgramMonitor
 
 def main():
-    menu = Menu()
-
-    while True:
-        menu.show_menu()
-        option = input("Seleccione una opción: ")
-        menu.execute_option(option)
+    program_monitor = ProgramMonitor()
+    program_monitor.start_monitoring()
 
 if __name__ == "__main__":
-    target_location = (-0.2295, -78.5243)  # Ejemplo de ubicación de destino (Quito, Ecuador)
-    program_monitor = ProgramMonitor(target_location)
-    program_monitor.start_monitoring()
     main()
